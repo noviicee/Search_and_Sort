8922f817f947aeb96bcff37086eee2847763efeb
daretohack <cseanamika3@gmail.com>
2021-02-07 00:14:01 +0530

    Added the Sorting Algos file

---
+++ Sorting Algos/Monkey Sort/random_demo.py
@@ -0,0 +1,21 @@
+#A demonstratio on how the random module will be used in Monkey Sort.
+
+import random
+a=[1,2,3,4,5] #a is any list
+random.shuffle(a)
+print(a)
+
+#Output-
+"""
+>random_demo.py
+[3, 1, 2, 4, 5]
+
+>random_demo.py
+[4, 3, 2, 5, 1]
+
+>random_demo.py
+[1, 2, 5, 4, 3]
+
+>
+"""
+#Everytime, it gives rise to a new list containing shuffled items from the given list.
