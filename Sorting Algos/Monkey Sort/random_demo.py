31ef93afb9865238e82619d4d82fa6dbf62a2871
Novice :) <72334601+noviicee@users.noreply.github.com>
2021-02-26 22:55:10 +0530

    Update random_demo.py

--- Sorting Algos/Monkey Sort/random_demo.py
+++ Sorting Algos/Monkey Sort/random_demo.py
@@ -1,21 +1,30 @@
-#A demonstratio on how the random module will be used in Monkey Sort.
+#A demonstration on how the random module is generally used and will be used in Monkey Sort.
 
 import random
-a=[1,2,3,4,5] #a is any list
+a=[1,2,3,4,5]
+
+#a is any list
+
 random.shuffle(a)
 print(a)
 
 #Output-
 """
->random_demo.py
+>>> random_demo.py
 [3, 1, 2, 4, 5]
 
->random_demo.py
+>>> random_demo.py
 [4, 3, 2, 5, 1]
 
->random_demo.py
+>>> random_demo.py
 [1, 2, 5, 4, 3]
 
->
 """
+
+if __name__ == "__main__":
+
+    import doctest
+
+    doctest.testmod()
+
 #Everytime, it gives rise to a new list containing shuffled items from the given list.
